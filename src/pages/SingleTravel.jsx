aeeb494847f7d7e43b55919e5f5ca91ee6a51b7e
alexciccarese <alex.ciccarese@gmail.com>
2025-04-24 16:43:04 +0200

    Refactor SingleTravel component layout and improve console logging

--- src/pages/SingleTravel.jsx
+++ src/pages/SingleTravel.jsx
@@ -3,37 +3,21 @@ import travels from "../data/db_travels";
 
 export default function SingleTravel() {
   const { id } = useParams();
-  console.log(id);
+  console.log("ID from params:", id);
+
+  // Usa .find() per ottenere il viaggio corrispondente
+  const travel = travels.find((travel) => travel.id === Number(id));
 
 
   return (
-    <main className="container-fluid mt-4 d-flex justify-content-center">
-
-      {/* {travels.map(travel => (
-        <div key={travel.id}>
-          <div className="background">
-            <h1>{travel.destination}</h1>
-          </div>
-        </div>
-      ))} */}
-
-      <div className="card mb-3" style={{maxWidth: "540px"}}>
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img src="..." className="img-fluid rounded-start" alt="..."/>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
-            </div>
-          </div>
+    <>
+      <div className="square_image position-relative">
+        <img src="https://placehold.co/600x400.png" alt={travel.destination} className="img-fluid d-flex position-relative" />
+        <div className="position-absolute top-50 start-50 translate-middle text-center text-dark">
+          <h1 className="position-absolute">ssvsdv</h1>
         </div>
       </div>
 
-
-
-    </main>
+    </>
   );
 }
