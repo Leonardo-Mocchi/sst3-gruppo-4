5fbf6ca88d24f8c55425ba5000f7aafb06ebcaaf
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-25 12:31:23 +0200

    Add NotFoundPage component and update routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DefaultLayout from './layouts/DefaultLayout'
 import HomePage from './pages/HomePage'
 import SingleTravel from './pages/SingleTravel'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
           <Route Component={DefaultLayout}>
             <Route path="/" Component={HomePage} />
             <Route path="/:id" Component={SingleTravel} />
+            <Route path="*" Component={NotFoundPage} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+export default function NotFoundPage() {
+    return (
+        <>
+            <main>
+                <h2 className="text-center my-5">404 Pagina non trovata</h2>
+            </main>
+        </>
+    )
+}
