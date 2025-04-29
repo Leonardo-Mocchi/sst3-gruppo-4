f243c94fdef2f323cde63c9eddcdc98c4b5b74d3
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-29 10:51:25 +0200

    Refactor NotFoundPage layout for improved structure and styling consistency

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom'
+
 export default function NotFoundPage() {
     return (
         <>
-            <main>
-                <h2 className="text-center my-5">404 Pagina non trovata</h2>
+            <main className='d-flex flex-column align-items-center justify-content-start pt-3'>
+
+            <h2 className="text-center my-5">404 Pagina non trovata</h2>
+
+                {/* Home button */}
+                <div className="d-flex justify-content-center my-5">
+                    <Link to={"/"} className='text-decoration-none text-dark'>
+                        <button className='btn btn-outline-dark mt-4'> <i className="bi bi-arrow-left"> </i> Torna alla Home </button>
+                    </Link>
+                </div>
+
             </main>
         </>
     )
