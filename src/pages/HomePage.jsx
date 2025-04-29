d93b37a5b2b889167249385da4cdd1e72f561173
mattiapennisi <mattiapennisi0@gmail.com>
2025-04-29 10:54:06 +0200

    Fix travel cards rendering order by reversing the map function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ export default function HomePage() {
                                 <CardStructure travel={travel} />
                             </Link>
                         </div>
-                    ))}
+                    )).reverse()}
                 </div>
             </div >
         </main >
